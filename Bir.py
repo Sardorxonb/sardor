57488c7c9a5b30c5d406399d277ca6fb44620406
Sardorxonb <87461560+Sardorxonb@users.noreply.github.com>
2021-07-24 22:08:50 +0500

    Update Bir.py

--- Bir.py
+++ Bir.py
@@ -1,1 +1,25 @@
-# sardor
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+
+API_TOKEN = '1764226729:AAGNHGHzHE6rmOChPotBYTvikfJC8HROaGI'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(bot=API_OKEN)
+
+db = Dispatcher(bot)
+
+@db.message_handler(cammands=['start', 'salom'])
+
+async def send_welcome(message: types.Message) :
+	
+	await message.reply("Assalomu alaykum")
+	
+	
+	
+
+if __name__ == '__main__':
+  executor.start_polling(dp, skip_updates=True)
+
